c8d00a3007dc85d9b68c833eba1f8200ca0634d0
Dino Wang <dino.cool@gmail.com>
2024-09-15 17:16:46 +0800

    add second server mode

---
+++ src/Max-Thread-Server-Worker/NamedPipeClient/Program.cs
@@ -0,0 +1,46 @@
+﻿// Create Named Pipe Client
+using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+
+namespace NamedPipeClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random(DateTime.Now.Millisecond);
+
+            while (true)
+            {
+                var thread = new Thread(WorkerThread);
+                thread.Start();
+
+                Thread.Sleep(random.Next(1, 100));
+            }
+        }
+
+        private static void WorkerThread(object? obj)
+        {
+            // Create named pipe client
+            using var pipeClient = new NamedPipeClientStream(".", "testpipe", PipeDirection.InOut);
+
+            // Connect to the server
+            pipeClient.Connect();
+
+            var taskId = Guid.NewGuid();
+
+            // Send request to the server
+            StreamWriter writer = new StreamWriter(pipeClient);
+            Console.WriteLine($"{taskId}: Send");
+            writer.WriteLine($"{taskId}");
+            writer.Flush();
+
+            // Read response from the server
+            StreamReader reader = new StreamReader(pipeClient);
+            string response = reader.ReadLine()!;
+            Console.WriteLine($"{taskId}: Response {response}");
+        }
+    }
+}
---
+++ src/Max-Thread-Server-Worker/NamedPipeServer/Program.cs
@@ -0,0 +1,67 @@
+﻿// Create named pipe server
+using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+
+namespace NamedPipeServer
+{
+    class Program
+    {
+        static bool run = true;
+
+        static int counter = 0;
+
+        static void Main(string[] args)
+        {
+            var random = new Random(DateTime.Now.Millisecond);
+
+            while (run)
+            {
+                var pipeServer = new NamedPipeServerStream("testpipe", PipeDirection.InOut, 254);
+                pipeServer.WaitForConnection();
+
+                var thread = new Thread(() =>
+                {
+                    var workerId = Interlocked.Increment(ref counter);
+                    var threadPipeServer = pipeServer;
+
+                    try
+                    {
+                        using StreamReader reader = new StreamReader(threadPipeServer);
+                        string request = reader.ReadLine()!;
+                        Console.WriteLine($"Worker {workerId}: Get {request}");
+
+                        // Process
+                        Thread.Sleep(random.Next(200, 3000));
+
+                        // Send response to the client
+                        using StreamWriter writer = new StreamWriter(threadPipeServer);
+                        writer.WriteLine(request);
+                        writer.Flush();
+
+                        Console.WriteLine($"Worker {workerId}: Responsed");
+
+                    }
+                    catch (IOException ex) when (ex.Message.Contains("Broken pipe")
+                                                || ex.Message.Contains("Pipe is broken"))
+                    {
+                        Console.WriteLine($"Worker {workerId}: {ex.Message}");
+                    }
+                    finally
+                    {
+                        if (threadPipeServer.IsConnected)
+                        {
+                            threadPipeServer.Disconnect();
+                            threadPipeServer.Close();
+                            threadPipeServer = null;
+                        }
+                    }
+                });
+
+                thread.Start();
+            }
+        }
+    }
+}
+
